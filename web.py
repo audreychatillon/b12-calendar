7b00c6221986f34aea0bcd8338f31273aa2d6404
audreychatillon <audrey.chatillon@gmail.com>
2026-06-25 17:45:18 +0200

    update

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect, send_file, abort, g
 from datetime import date,timedelta
 import sqlite3
 import os
